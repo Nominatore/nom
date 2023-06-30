aaf9db0ffad41477633873d88fc23879dccf13f0
Nominatore <138215389+Nominatore@users.noreply.github.com>
2023-06-30 19:21:58 +0200

    Update nominatore.py

--- nominatore.py
+++ nominatore.py
@@ -8,10 +8,10 @@ async def on_ready():
 
 @bot.slash_command(description="Imposta nome, cognome e server del tuo personaggio.",
                    guild_ids=[1118403657784889404])
-async def setup(ctx, nome:str, cognome:str):
+async def nome(ctx, nome:str, cognome:str, server:str):
     
-    await ctx.author.edit(nick=f"{nome.capitalize()} {cognome.capitalize()}")
+    await ctx.author.edit(nick=f"{nome.capitalize()} {cognome.capitalize()} [{server.capitalize()}]")
     await ctx.author.add_roles(ctx.guild.get_role(1118622975483183154))
-    await ctx.respond("Nome cambiato con successo.")
-    
+    await ctx.respond("Nome cambiato con successo.", delete_after=5)
+
 bot.run('MTEyMjQ2MTM0NjkxOTg4Mjg0Mw.G1o-21.ydXTCVC0ekbTRfsavAmm1aueOOqRPT-YHkTL2I')
